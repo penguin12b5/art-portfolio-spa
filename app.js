3319b1ef9f930771e58ca6f32fe7626fb50de9b2
Oliver <forbiddenpenguin12b5@gmail.com>
2026-03-14 22:50:20 -0400

    Add zoom in/out for artwork detail

--- app.js
+++ app.js
@@ -497,6 +497,22 @@ function filterGalleryByCategory(category) {
 }
 
 // ---------- Lightbox ----------
+function resetZoom() {
+  const lightboxContent = document.querySelector('.lightbox__content');
+  const zoomBtn = document.getElementById('lightbox-zoom');
+  if (lightboxContent) {
+    lightboxContent.classList.remove('is-zoomed');
+  }
+  if (zoomBtn) {
+    zoomBtn.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
+      </svg>
+      Zoom In
+    `;
+  }
+}
+
 function openLightbox(index) {
   currentLightboxIndex = index;
   const artwork = filteredArtworks[index];
@@ -512,11 +528,13 @@ function openLightbox(index) {
 
   lightbox.classList.add('open');
   document.body.style.overflow = 'hidden';
+  resetZoom();
 }
 
 function closeLightbox() {
   lightbox.classList.remove('open');
   document.body.style.overflow = '';
+  resetZoom();
 }
 
 function navigateLightbox(direction) {
@@ -554,6 +572,30 @@ if (fullResBtn) {
       sendGtagEvent('full_resolution_view', {
         artwork_src: lightboxImage.src,
       });
+    }
+  });
+}
+
+const zoomBtn = document.getElementById('lightbox-zoom');
+const lightboxContent = document.querySelector('.lightbox__content');
+
+if (zoomBtn && lightboxContent) {
+  zoomBtn.addEventListener('click', () => {
+    const isNowZoomed = lightboxContent.classList.toggle('is-zoomed');
+    if (isNowZoomed) {
+      zoomBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM13 10H7" />
+        </svg>
+        Zoom Out
+      `;
+    } else {
+      zoomBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
+        </svg>
+        Zoom In
+      `;
     }
   });
 }
